39a9eca93f65f30a5232bbf140aa89e8535271ef
jaeyeong815 <jae0ee@naver.com>
2022-11-02 15:57:58 +0900

    feat: CategoryTag 컴포넌트 생성

---
+++ src/components/Main/Category/CategoryTag.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import color from '../../../styles/color';
+
+const CategoryTag = ({ select, text }) => {
+  return <StButton select={select}>{text}</StButton>;
+};
+
+export default CategoryTag;
+
+const StButton = styled.button`
+  background-color: ${({ select }) => (select ? color.black : color.gray)};
+  color: ${({ select }) => (select ? color.white : color.black)};
+
+  border: none;
+  border-radius: 65px;
+
+  width: 62 px;
+  height: 30px;
+
+  margin-right: 8px;
+  padding: 5px 18px;
+
+  font-size: 16px;
+  font-weight: 500;
+
+  cursor: pointer;
+`;
