a906bff7b1a907a04f4fe7ae2e1a121ad57d7396
jaeyeong815 <jae0ee@naver.com>
2022-11-02 22:25:51 +0900

    feat: 전체 자동차 목록 불러오는 getCarList api 작성

--- src/apis/apis.js
+++ src/apis/apis.js
@@ -1,1 +1,6 @@
-// .gitkeep
+import axios from 'axios';
+
+export const getCarList = async () => {
+  const { payload } = await (await axios.get(`https://preonboarding.platdev.net/api/cars`)).data;
+  return payload;
+};
