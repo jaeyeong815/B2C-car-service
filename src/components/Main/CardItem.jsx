3b472c08c013b05994613500fdff77d4929fac67
jaeyeong815 <jae0ee@naver.com>
2022-11-02 16:26:28 +0900

    feat: CardItem 컴포넌트 생성

---
+++ src/components/Main/CardItem.jsx
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+import color from '../../styles/color';
+
+// TODO props = {brand, name, classType, fuelType, amount}
+const CardItem = () => {
+  return (
+    <StCardItem>
+      <div className="contentWrapper">
+        <StTitle>
+          <p>기아</p>
+          <p>EV6</p>
+        </StTitle>
+        <StDescription>
+          <p>중형 / 전기</p>
+          <p>월 600,000 원 부터</p>
+        </StDescription>
+      </div>
+      <StImage />
+    </StCardItem>
+  );
+};
+
+export default CardItem;
+
+const StCardItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 0 20px;
+
+  height: 120px;
+  border-bottom: 1.5px ${color.black} solid;
+`;
+
+const StTitle = styled.div`
+  margin-bottom: 10px;
+  p {
+    font-weight: 800;
+  }
+`;
+
+const StDescription = styled.div``;
+
+const StImage = styled.div`
+  background-color: ${color.gray};
+  width: 170px;
+  height: 80px;
+`;
