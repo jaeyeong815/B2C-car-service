0504398ae41edaf26bf89ed2b8b426fa71f9e952
jaeyeong815 <jae0ee@naver.com>
2022-11-03 01:30:07 +0900

    feat: 카테고리에 맞는 차량 데이터를 반환해주는 api 작성

--- src/apis/apis.js
+++ src/apis/apis.js
@@ -4,3 +4,12 @@ export const getCarList = async () => {
   const { payload } = await (await axios.get(`https://preonboarding.platdev.net/api/cars`)).data;
   return payload;
 };
+
+export const getFilterCarList = async (filter, condition) => {
+  if (condition === '전체') {
+    const { payload } = await (await axios.get(`https://preonboarding.platdev.net/api/cars`)).data;
+    return payload;
+  }
+  const { payload } = await (await axios.get(`https://preonboarding.platdev.net/api/cars?${filter}=${condition}`)).data;
+  return payload;
+};
