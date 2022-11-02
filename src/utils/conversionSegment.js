e3048f8e43b2a403bbf61bf1b89c1408eae5bbb5
jaeyeong815 <jae0ee@naver.com>
2022-11-02 22:24:14 +0900

    feat: 서버에서 받아온 데이터 중 각 segment에 맞는 값 반환해주는 함수

---
+++ src/utils/conversionSegment.js
@@ -0,0 +1,16 @@
+const conversionSegment = (segment) => {
+  switch (segment) {
+    case 'C':
+      return '소형';
+    case 'D':
+      return '중형';
+    case 'E':
+      return '대형';
+    case 'SUV':
+      return 'SUV';
+    default:
+      return null;
+  }
+};
+
+export default conversionSegment;
