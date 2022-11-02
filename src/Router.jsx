3d2b9756e85ac19702c5e0cc3ea8a32de7ce7ff8
jaeyeong815 <jae0ee@naver.com>
2022-11-02 23:48:39 +0900

    refactor: detail페이지 라우트 변경

--- src/Router.jsx
+++ src/Router.jsx
@@ -8,7 +8,7 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<Main />} />
-        <Route path="/detail" element={<Detail />} />
+        <Route path="/detail/:id" element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
