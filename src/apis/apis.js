61478530ec3ba179b9939644e8639138891fc624
dldnjswns31 <dldnjswns32@gmail.com>
2022-11-04 00:34:18 +0900

    feature: axios instance 생성 및 apis 로직 수정

--- src/apis/apis.js
+++ src/apis/apis.js
@@ -1,15 +1,23 @@
-import axios from 'axios';
+import axiosInstance from '../utils/axios';
 
-export const getCarList = async () => {
-  const { payload } = await (await axios.get(`https://preonboarding.platdev.net/api/cars`)).data;
-  return payload;
+const apis = {
+  getCarList: async () => {
+    const { data } = await axiosInstance();
+    return data.payload;
+  },
+  getFilteredCarList: async (filter, condition) => {
+    if (condition === '전체') {
+      const { data } = await axiosInstance();
+      return data.payload;
+    }
+
+    const { data } = await axiosInstance({
+      params: {
+        [filter]: condition,
+      },
+    });
+    return data.payload;
+  },
 };
 
-export const getFilterCarList = async (filter, condition) => {
-  if (condition === '전체') {
-    const { payload } = await (await axios.get(`https://preonboarding.platdev.net/api/cars`)).data;
-    return payload;
-  }
-  const { payload } = await (await axios.get(`https://preonboarding.platdev.net/api/cars?${filter}=${condition}`)).data;
-  return payload;
-};
+export default apis;
