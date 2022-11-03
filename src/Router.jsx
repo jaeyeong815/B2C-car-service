f6d93006e209eb116fa72447e7805131c667563d
dldnjswns31 <dldnjswns32@gmail.com>
2022-11-04 02:22:15 +0900

    feat: HelmetProvider 위치 변경
    - index.js 파일에서 App을 감싸주게 함

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,18 +3,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Detail from './pages/Detail';
 import Header from './components/common/Header';
-import { HelmetProvider } from 'react-helmet-async';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <HelmetProvider>
-        <Header />
-        <Routes>
-          <Route path="*" element={<Main />} />
-          <Route path="/detail/:id" element={<Detail />} />
-        </Routes>
-      </HelmetProvider>
+      <Header />
+      <Routes>
+        <Route path="*" element={<Main />} />
+        <Route path="/detail/:id" element={<Detail />} />
+      </Routes>
     </BrowserRouter>
   );
 };
