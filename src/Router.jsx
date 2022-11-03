90ec3f934e8ef449f7eed32ec0c1385a4fe2553f
seungyeon-rr <syryu926@gmail.com>
2022-11-03 22:42:12 +0900

    feat(SEO): SEO 메타태그 구현

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Detail from './pages/Detail';
+import { HelmetProvider } from 'react-helmet-async';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<Main />} />
-        <Route path="/detail/:id" element={<Detail />} />
-      </Routes>
+      <HelmetProvider>
+        <Routes>
+          <Route path="*" element={<Main />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
+      </HelmetProvider>
     </BrowserRouter>
   );
 };
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,9 +1,11 @@
 import Header from '../components/Header';
 import CarDetail from '../components/Detail/CarDetail';
+import MetaTag from '../components/MetaTag';
 
 const Detail = () => {
   return (
     <div className="detail">
+      <MetaTag />
       <Header icon={true} text="차량상세" />
       <CarDetail />
     </div>
