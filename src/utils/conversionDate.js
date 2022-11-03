c7ae2d94206ef4e65b46a837964b9db64252dc70
jaeyeong815 <jae0ee@naver.com>
2022-11-03 18:55:39 +0900

    refactor: 요일 변경 로직 변경

--- src/utils/conversionDate.js
+++ src/utils/conversionDate.js
@@ -1,31 +1,11 @@
 const conversionDate = (date) => {
+  const day = ['일', '월', '화', '수', '목', '금', '토'];
   const newDate = new Date(date);
   const month = newDate.getMonth() + 1;
   const dayOfMonth = newDate.getDate();
-  const dayOfWeek = getDayOfWeek(newDate.getDay());
+  const dayOfWeek = day[newDate.getDay()];
 
   return `${month}월 ${dayOfMonth}일 (${dayOfWeek}) 부터`;
-};
-
-const getDayOfWeek = (dayOfWeek) => {
-  switch (dayOfWeek) {
-    case 0:
-      return '일';
-    case 1:
-      return '월';
-    case 2:
-      return '화';
-    case 3:
-      return '수';
-    case 4:
-      return '목';
-    case 5:
-      return '금';
-    case 6:
-      return '토';
-    default:
-      return null;
-  }
 };
 
 export default conversionDate;
