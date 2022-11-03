d85e980976d445518db6d7e14f0375547d435397
jaeyeong815 <jae0ee@naver.com>
2022-11-03 18:45:34 +0900

    fix: Detail페이지로 바로 가기 가능하도록 수정

--- src/components/Detail/CarDetail.jsx
+++ src/components/Detail/CarDetail.jsx
@@ -1,50 +1,55 @@
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
-import { useSelectCar } from '../../context/CarContext';
+import { useSelectCar, useLoding } from '../../context/CarContext';
 import comma from '../../utils/comma';
 import conversionSegment from '../../utils/conversionSegment';
 import conversionFuelType from '../../utils/conversionFuelType';
 import conversionDate from '../../utils/conversionDate';
 import ListHeader from './ListHeader';
 import ListContent from './ListContent';
+import Guide from '../Guide';
 
 const CarDetail = () => {
   const { id } = useParams();
+  const isLoding = useLoding();
   const getSelectCarInfo = useSelectCar();
   const carData = getSelectCarInfo(id)[0];
-  const { attribute, insurance, additionalProducts, startDate, amount } = carData;
 
-  return (
-    <StCarDetail>
-      <StCarImage src={attribute.imageUrl} />
-      <StTitle>
-        <StBrand>{attribute.brand}</StBrand>
-        <StName>{attribute.name}</StName>
-      </StTitle>
-      <StPrice>월 {comma(amount)} 원</StPrice>
-      <div className="carInfo">
-        <ListHeader text="차량 정보" />
-        <ListContent name="차종" description={conversionSegment(attribute.segment)} />
-        <ListContent name="연료" description={conversionFuelType(attribute.fuelType)} />
-        <ListContent name="이용 가능일" description={conversionDate(startDate)} />
-      </div>
-      {insurance && (
-        <div className="insurance">
-          <ListHeader text="보험" />
-          {insurance.map((data, index) => {
-            return <ListContent key={index} name={data.name} description={data.description} />;
-          })}
+  return isLoding ? (
+    <Guide text="불러오는 중" />
+  ) : (
+    carData && (
+      <StCarDetail>
+        <StCarImage src={carData.attribute.imageUrl} />
+        <StTitle>
+          <StBrand>{carData.attribute.brand}</StBrand>
+          <StName>{carData.attribute.name}</StName>
+        </StTitle>
+        <StPrice>월 {comma(carData.amount)} 원</StPrice>
+        <div className="carInfo">
+          <ListHeader text="차량 정보" />
+          <ListContent name="차종" description={conversionSegment(carData.attribute.segment)} />
+          <ListContent name="연료" description={conversionFuelType(carData.attribute.fuelType)} />
+          <ListContent name="이용 가능일" description={conversionDate(carData.startDate)} />
         </div>
-      )}
-      {additionalProducts && (
-        <div className="additionalProducts">
-          <ListHeader text="추가상품" />
-          {additionalProducts.map((data, index) => {
-            return <ListContent key={index} name={data.name} description={`월 ${comma(data.amount)} 원`} />;
-          })}
-        </div>
-      )}
-    </StCarDetail>
+        {carData.insurance && (
+          <div className="insurance">
+            <ListHeader text="보험" />
+            {carData.insurance.map((data, index) => {
+              return <ListContent key={index} name={data.name} description={data.description} />;
+            })}
+          </div>
+        )}
+        {carData.additionalProducts && (
+          <div className="additionalProducts">
+            <ListHeader text="추가상품" />
+            {carData.additionalProducts.map((data, index) => {
+              return <ListContent key={index} name={data.name} description={`월 ${comma(data.amount)} 원`} />;
+            })}
+          </div>
+        )}
+      </StCarDetail>
+    )
   );
 };
 
