3686a7d48c09db806b0549bd706f0294d1560164
dldnjswns31 <dldnjswns32@gmail.com>
2022-11-03 21:34:25 +0900

    feature: apis 파일 import axios 삭제

--- src/apis/apis.js
+++ src/apis/apis.js
@@ -1,19 +1,4 @@
-import axios from 'axios';
 import axiosInstance from '../utils/axios';
-
-// export const getCarList = async () => {
-//   const { payload } = await (await axios.get(`https://preonboarding.platdev.net/api/cars`)).data;
-//   return payload;
-// };
-
-// export const getFilterCarList = async (filter, condition) => {
-//   if (condition === '전체') {
-//     const { payload } = await (await axios.get(`https://preonboarding.platdev.net/api/cars`)).data;
-//     return payload;
-//   }
-//   const { payload } = await (await axios.get(`https://preonboarding.platdev.net/api/cars?${filter}=${condition}`)).data;
-//   return payload;
-// };
 
 const apis = {
   getCarList: async () => {
